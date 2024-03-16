406bec03c91a3442c5e5ef78f35f2ad31313268d
Konstantin Tutsch <mail@konstantintutsch.com>
2024-03-16 20:20:55 +0100

    Cleanup test

--- test.c
+++ test.c
@@ -14,7 +14,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #include <stdio.h>
 
 #include "zarg.h"
@@ -22,26 +21,23 @@
 int main(int   argc,
          char *argv[])
 {
-    Flag help = {"help", 'h', 'b', "Help dialogue"};
-    Flag num = {"number", 'n', 'i', "Print number"};
-    Flag str = {"string", 's', 's', "Print string"};
+    Flag help = {"help",   'h', 'b', "Help dialogue"};
+    Flag num  = {"number", 'n', 'i', "Print number"};
+    Flag str  = {"string", 's', 's', "Print string"};
 
-    int helps = argument_count(argv, &help);
-    int numbers = argument_count(argv, &num);
-    int strings = argument_count(argv, &str);
-
-    printf("Helps given: %d\n", helps);
-    printf("Numbers given: %d\n", numbers);
-    printf("Strings given: %d\n", strings);
+    if (argument_count(argv, &help) > 0)
+    {
+        printf(" --help,\t-h\tThis dialogue\n --number,\t-n\tPass a number\n --string,\t-s\tPass a string\n");
+        return 0;
+    }
     
     int *num_list = (int *)argument_value(argv, &num);
-    for (int i = 0; i < numbers; i++)
+    for (int i = 0; i < argument_count(argv, &num); i++)
         printf("Number %d: %d\n", i, num_list[i]);
     
     char **str_list = (char **)argument_value(argv, &str);
-    for (int i = 0; i < strings; i++)
+    for (int i = 0; i < argument_count(argv, &str); i++)
         printf("String %d: %s\n", i, str_list[i]);
-
 
     return 0;
 }
